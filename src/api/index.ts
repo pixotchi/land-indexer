7a26bd99061c1f0895ca9b483155999be424e756
7118-eth <7118.eth@proton.me>
2024-09-24 17:52:40 +0200

    Remove obsolete event handler code
    Commented out unused event handler code for LandMinted event to clean up the codebase. This ensures that only relevant and necessary code is present in the project.

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,3 +3,9 @@ import { graphql } from "@ponder/core";
 
 ponder.use("/graphql", graphql());
 ponder.use("/", graphql());
+
+// ponder.on("LandContract:LandMinted", async (ctx) => {
+//   // const { event, block } = ctx;
+//   // const { tokenId, to } = event.args;
+//   // const land = await ctx.db.select().from(lands).where(eq(lands.tokenId, tokenId));
+// });

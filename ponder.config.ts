32ac1894159052593af401f8f29b88f230334447
7118-eth <7118.eth@proton.me>
2024-09-24 17:52:52 +0200

    Update configuration for Base Sepolia network support
    Revised the network configuration to switch from Mainnet to Base Sepolia, added load balancing and rate limiting for HTTP requests, and updated contract details accordingly. The changes aim to improve reliability and performance of network operations.

--- ponder.config.ts
+++ ponder.config.ts
@@ -1,21 +1,29 @@
-import { createConfig } from "@ponder/core";
-import { http } from "viem";
+import {createConfig, loadBalance, rateLimit} from "@ponder/core";
+import {getAddress, http, webSocket} from "viem";
 
-import { ExampleContractAbi } from "./abis/ExampleContractAbi";
+import { abi } from "./abis/abi";
+//import { baseSepolia } from "viem/chains";
 
 export default createConfig({
   networks: {
-    mainnet: {
-      chainId: 1,
-      transport: http(process.env.PONDER_RPC_URL_1),
+    baseSepolia: {
+      chainId: 84532,
+      transport: loadBalance([
+        //http("https://cloudflare-eth.com"),
+        //http("https://eth-mainnet.public.blastapi.io"),
+        //webSocket("wss://ethereum-rpc.publicnode.com"),
+        rateLimit(http(process.env.PONDER_RPC_URL_84532), { requestsPerSecond: 49 }),
+      ]),
+      //transport: rateLimit(http("https://rpc.ankr.com/eth"), { requestsPerSecond: 5 }),//http(process.env.PONDER_RPC_URL_1)
+
     },
   },
   contracts: {
-    ExampleContract: {
-      network: "mainnet",
-      abi: ExampleContractAbi,
-      address: "0x0000000000000000000000000000000000000000",
-      startBlock: 1234567,
+    LandContract: {
+      network: "baseSepolia",
+      abi: abi,
+      address: getAddress(process.env.PONDER_LAND_CONTRACT_ADDRESS_84532 as string),
+      startBlock: 15709970,
     },
   },
 });
